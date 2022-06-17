48bdc092c4a01703450bca65a78b4591f06e48c5
Tristan Geronimo <tristanpgeronimo@gmail.com>
2022-06-17 09:53:35 +0200

    Add files via upload

---
+++ API-Interceptor01/src/main/java/it/triexercise/APIInterceptor01/ApiInterceptor01Application.java
@@ -0,0 +1,13 @@
+package it.triexercise.APIInterceptor01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiInterceptor01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiInterceptor01Application.class, args);
+	}
+
+}
